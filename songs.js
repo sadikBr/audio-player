bf6109f16181e4a404531035cd1e6b40f87fec42
sadikBr <sadbrah@gmail.com>
2021-04-14 12:36:49 +0000

    first commit

---
+++ songs.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    source: './songs/source/Katy Perry - Teenage Dream.webm',
+    thumbnail: './songs/images/Katty Pery - Teenage Dream.jpeg',
+    artist: 'Katy Perry',
+    title: 'Teenage Dream',
+  },
+  {
+    source: './songs/source/Lynda feat Soolking - Luna.webm',
+    thumbnail: './songs/images/Soolking - Luna.jpeg',
+    artist: 'Lynda feat. Soolking',
+    title: 'Luna',
+  },
+  {
+    source: './songs/source/Youssef Shems feat. Ramos - MAYEMI.webm',
+    thumbnail: './songs/images/Youssef Shems - mayemi.jpeg',
+    artist: 'Youssef Shems feat. Ramos',
+    title: 'Mayemi',
+  },
+];
